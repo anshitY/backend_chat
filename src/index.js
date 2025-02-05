a489913ebfd1e1c2def608dd27cfb91007c72a2d
anshit <21uec026@lnmiit.ac.in>
2025-02-05 21:13:36 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -2,19 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
-
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-
-const PORT = process.env.PORT;
-const __dirname = path.resolve();
+const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -29,14 +24,16 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
-server.listen(PORT, () => {
-  console.log("server is running on PORT:" + PORT);
-  connectDB();
-});
+// Connect to the database
+connectDB();
+
+// Export the Express app for Vercel
+export default app;
